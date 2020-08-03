6f276e49fceec62fdacd34b298b6139984bb5b32
Thanh <quang.thanh.ta@pr1meti.de>
2020-08-03 19:16:34 +0200

    Formatting existing files

--- src/main/java/de/pr1meti/ls4pde/server/ProcessingLangServer.java
+++ src/main/java/de/pr1meti/ls4pde/server/ProcessingLangServer.java
@@ -1,10 +1,5 @@
 package de.pr1meti.ls4pde.server;
 
-import org.eclipse.lsp4j.*;
-import org.eclipse.lsp4j.services.*;
-
-import de.pr1meti.ls4pde.server.formatting.AutoFormatter;
-
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,7 +7,30 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class ProcessingLangServer implements LanguageClientAware, WorkspaceService, TextDocumentService, LanguageServer {
+import org.eclipse.lsp4j.DidChangeConfigurationParams;
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
+import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
+import org.eclipse.lsp4j.DidCloseTextDocumentParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.DocumentFormattingParams;
+import org.eclipse.lsp4j.InitializeParams;
+import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.ServerInfo;
+import org.eclipse.lsp4j.TextEdit;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.lsp4j.services.TextDocumentService;
+import org.eclipse.lsp4j.services.WorkspaceService;
+
+import de.pr1meti.ls4pde.server.formatting.AutoFormatter;
+
+public class ProcessingLangServer
+        implements LanguageClientAware, WorkspaceService, TextDocumentService, LanguageServer {
 
     LanguageClient client;
 
@@ -103,17 +121,19 @@ public class ProcessingLangServer implements LanguageClientAware, WorkspaceServi
                 textEdit.setNewText(after);
 
                 Range selectAll = new Range();
-                selectAll.setStart(new Position(0,0));
+                selectAll.setStart(new Position(0, 0));
 
                 // Determining the End position for selection Range
-                long lastIdx = before.lines()
-                    .count();
-                int lastCharAtLastLine = before.lines()
-                    .skip(lastIdx)
-                    .mapToInt(String::length)
-                    .findFirst()
-                    .orElse(50); // Random range which likely encompasses the last char
-                selectAll.setEnd(new Position((int)lastIdx, lastCharAtLastLine));
+                long lastIdx = before.lines().count();
+                int lastCharAtLastLine = before.lines().skip(lastIdx).mapToInt(String::length).findFirst().orElse(50); // Random
+                                                                                                                       // range
+                                                                                                                       // which
+                                                                                                                       // likely
+                                                                                                                       // encompasses
+                                                                                                                       // the
+                                                                                                                       // last
+                                                                                                                       // char
+                selectAll.setEnd(new Position((int) lastIdx, lastCharAtLastLine));
                 textEdit.setRange(selectAll);
 
                 return Collections.singletonList(textEdit);
