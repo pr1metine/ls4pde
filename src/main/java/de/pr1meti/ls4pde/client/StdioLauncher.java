3334da9dc1f5437b56a042fa7388ff838de503be
Thanh <quang.thanh.ta@pr1meti.de>
2020-09-13 13:11:26 +0200

    Revised the saving of log files - Pass a folder path as a command line param for the logs to be saved to - Log file names follow this format: "yyyy.MM.dd-HH.mm.ss.SSS-ls4pde-debug.log"

--- src/main/java/de/pr1meti/ls4pde/client/StdioLauncher.java
+++ src/main/java/de/pr1meti/ls4pde/client/StdioLauncher.java
@@ -4,6 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
@@ -21,7 +24,17 @@ import de.pr1meti.ls4pde.server.ProcessingLangServer;
  */
 public class StdioLauncher {
 
+    public static String rootPath = Paths.get(
+            System.getProperty("user.home"),
+            "projects",
+            "vscode-ls4pde",
+            "server_logs"
+    ).toString();
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        if (args.length >= 1) {
+            rootPath = args[0];
+        }
         LogManager.getLogManager().reset();
         Logger globalLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
         globalLogger.setLevel(Level.OFF);
@@ -31,9 +44,10 @@ public class StdioLauncher {
     private static void startServer(InputStream in, OutputStream out) throws ExecutionException, InterruptedException {
 
         ProcessingLangServer server = new ProcessingLangServer();
-        Launcher<LanguageClient> launcher = null;
+        Launcher<LanguageClient> launcher;
         try {
-            PrintWriter writer = new PrintWriter("C:\\Users\\derth\\projects\\vscode-ls4pde\\debug.log");
+            String fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd-HH.mm.ss.SSS")) + "-ls4pde-debug.log";
+            PrintWriter writer = new PrintWriter(Paths.get(rootPath, fileName).toString());
             launcher = LSPLauncher.createServerLauncher(server, in, out, true, writer);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
